d3aa057339bca87a8c03fd263d8366546410c593
[jala-foundation-student04] <[student4@jala-foundation.org]>
2020-12-09 11:56:02 -0400

    optimizacion

--- src/main/java/OptimizacionHooke.java
+++ src/main/java/OptimizacionHooke.java
@@ -1,3 +1,7 @@
+
+//import static SimulacionSinEspera.costoInventario;
+//import static SimulacionSinEspera.lista;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -42,6 +46,81 @@ public class OptimizacionHooke {
         }
         System.out.println("######################################");
         System.out.println("EL COSTO MAS OPTIMO ES: " + valorMinimo + " CON ORDEN IGUAL A : " + OrdenPivote + " Y REORDEN IGUAL A : " + ReordenPivote);
+
+    }
+       public double Optimi(int q, int R, int N , int unidadesIniciales , int costoFaltante , int costoOrden , int CostoInventario) {
+        Demanda demanda = new Demanda();
+        TiempoEntrega tiempoDeEntrega = new TiempoEntrega();
+        double inventarioInicial[];
+        inventarioInicial = new double[N + 1];
+
+        double inventarioFinal[];
+        inventarioFinal = new double[N];
+
+        double promedio[];
+        promedio = new double[N];
+
+        double inversionInicial = unidadesIniciales;
+
+        inventarioInicial[0] = inversionInicial;
+
+        double costoTotal = 0;
+        double banderaUno = 0; // mes en el cual llega el pedido 
+        double banderaDos = 0; //pedido en curso
+
+        double pedido = 0; //cantidad de pedidos
+        double faltante = 0; //cantidad de faltantes
+
+        for (int mes = 0; mes < N; mes++) {
+
+            if (mes == banderaUno && banderaDos == 1) {
+                inventarioInicial[mes] = inventarioInicial[mes] + q;
+
+                banderaDos = 0;
+            }
+
+            double aux = demanda.calcular(mes);
+
+            inventarioFinal[mes] = inventarioInicial[mes] - aux;
+            inventarioInicial[mes + 1] = inventarioFinal[mes];
+
+            if (inventarioFinal[mes] < 0) {
+                faltante = faltante + inventarioFinal[mes];
+
+                inventarioInicial[mes + 1] = inventarioFinal[mes];
+                promedio[mes] = inventarioInicial[mes] / 2 * inventarioInicial[mes] / aux;
+                inventarioFinal[mes] = 0;
+
+            } else {
+                promedio[mes] = (inventarioInicial[mes] + inventarioFinal[mes]) / 2;
+
+            }
+
+            if (inventarioInicial[mes] < R && banderaDos == 0) {
+
+                banderaUno = mes + tiempoDeEntrega.calcular();
+                pedido = pedido + 1;
+                banderaDos = 1;
+
+            }
+
+        }
+
+        int total = 0;
+        //total pedidos
+        for (int contador = 0; contador < promedio.length; contador++) {
+            total += promedio[contador];
+
+        }
+        double ordenar = costoOrden * pedido;
+        //costos
+        // System.out.println("El COSTO DE ORDENARMIENTO ES DE: " + ordenar + "Bs");
+        double inventario = total * CostoInventario / N;
+        // System.out.println("El COSTO DEL INVENTARIO ES DE: " + inventario + "Bs");
+        double falto = costoFaltante * faltante * (-1);
+        // System.out.println("El COSTO DEL FALTANTE ES DE: " + falto + "Bs");
+        costoTotal = ordenar + inventario + falto;
+        return costoTotal;
 
     }
 }
